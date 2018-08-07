52f5570196433573cbc373e9f5e0a7cf3d4ee4a6
Anand Patel <anandpatel@braindo-anandp.local>
2018-08-07 16:45:45 -0400

    setup rudimentry form validation and clearing

--- js/main.js
+++ js/main.js
@@ -1,18 +1,50 @@
 $(document).ready(function () {
+
+    emailReg = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
     $("#rsvp-form").submit(function (e) {
         e.preventDefault();
-        console.log('rsvp!');
-        $("#rsvp-input-name").addClass("error");
-        $("#rsvp-input-email").addClass("error");
-        $("#rsvp-input-company").addClass("error");
-        $("#rsvp-input-title").addClass("error");
+
+        nameField = document.getElementById("rsvp-input-name").value;
+        emailField = document.getElementById("rsvp-input-email").value;
+        companyField = document.getElementById("rsvp-input-company").value;
+        titleField = document.getElementById("rsvp-input-title").value;
+
+        if(nameField && emailField && companyField && titleField) {
+            if (emailField.match(emailReg)) {
+                $("#rsvp-form").removeClass("error");
+                $("#rsvp-form").trigger("reset");
+                return
+            }
+        }
+        $("#rsvp-form").addClass("error");
     });
+
     $("#sponsor-form").submit(function (e) {
         e.preventDefault();
-        console.log('sponsor!');
+        emailField = document.getElementById("sponsor-input-email").value;
+        if(emailField){
+            if(emailField.match(emailReg)){
+                $("#sponsor-form").removeClass("error");
+                $("#sponsor-form").trigger('reset');
+                return;
+            }
+        }
+        $("#sponsor-form").addClass("error");
+        console.log('Error State due to empty')
     });
+
     $("#footer-form").submit(function (e) {
         e.preventDefault();
-        console.log('footer!');
+        emailField = document.getElementById("footer-input-email").value;
+        if(emailField){
+            if(emailField.match(emailReg)){
+                $("#footer-form").removeClass("error");
+                $("#footer-form").trigger('reset');
+                return;
+            }
+        }
+        $("#footer-form").addClass("error");
+        console.log('Error State due to empty')
     });
 });
